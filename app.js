38125e01310ab5a147ef7523222f377c717fb8c4
WangRping <r17369@gmail.com>
2023-05-09 21:41:52 +0800

    feat:add: body-parser /create router update:index.hbs

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const port = 3000
 const app = express()
 
 if (!process.env.NODE_ENV !== 'produciton') { require('dotenv').config() }
 
-
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/create', (req, res) => {
+  const url = req.body.url
+  console.log(url)
   res.render('index')
 })
 
