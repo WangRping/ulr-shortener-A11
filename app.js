59e64d4f5aac40eb88c22920276d80756548d3c8
WangRping <r17369@gmail.com>
2023-05-09 21:15:50 +0800

    feat:add mongoose & url.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 const port = 3000
 const app = express()
+
+if (!process.env.NODE_ENV !== 'produciton') { require('dotenv').config() }
+
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
